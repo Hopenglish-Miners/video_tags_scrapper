8605768381972b61b46f034056cb464311c57579
eduardo-salazar <esalazar922@gmail.com>
2016-12-03 15:39:40 +0800

    Adding spec and gemfile to project:

---
+++ spec/connection_spec.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+
+describe Scrapper do
+  before do
+    # Init Capybara
+    @meme = Meme.new
+  end
+
+  describe "scrapper connection" do
+    it "can visit index page" do
+
+    end
+  end
+end
