c5b67af5df910add7578a0bf83ae8a8e5f3715cc
eduardo-salazar <esalazar922@gmail.com>
2016-12-03 16:39:05 +0800

    Added test to scrapper

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -1,14 +1,14 @@
 require 'minitest/autorun'
+require_relative '../scrapper.rb'
 
 describe Scrapper do
   before do
-    # Init Capybara
-    @meme = Meme.new
+    @scrapper = Scrapper.new
   end
-
   describe "scrapper connection" do
     it "can visit index page" do
-
+      home = @scrapper.visit('/')
+      home.status_code = 200
     end
   end
 end
